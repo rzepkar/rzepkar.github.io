bfb98557a11907b865b14f1c92b722f7252fbd9c
Rio Rzepka <rio.rzepka@gmail.com>
2025-04-13 12:02:04 +0200

    Update api.py

--- api.py
+++ api.py
@@ -169,11 +169,8 @@ def get_windenergieanlagen():
     except Exception as e:
         print("Fehler in /get_windenergieanlagen:", e)
         return {"error": str(e)}
-        
-@app.get("/mvt/test")
-def test():
-    return {"status": "online"}
-
+   
+    
 @app.get("/mvt/buildings/{z}/{x}/{y}")
 def get_mvt(z: int, x: int, y: int):
     conn = get_db_connection()
@@ -190,14 +187,14 @@ def get_mvt(z: int, x: int, y: int):
             name,
             height,
             ST_AsMVTGeom(
-                b.geom,
+                b.geom_3857,
                 bounds.geom,
                 4096,
                 0,
                 true
             ) AS geom
         FROM buildings b, bounds
-        WHERE ST_Intersects(b.geom, bounds.geom)
+        WHERE ST_Intersects(b.geom_3857, bounds.geom)
     )
     SELECT ST_AsMVT(mvtgeom, 'buildings_layer', 4096, 'geom') FROM mvtgeom;
     """
@@ -206,6 +203,9 @@ def get_mvt(z: int, x: int, y: int):
     row = cur.fetchone()
     cur.close()
     conn.close()
+
+    return Response(content=row[0], media_type="application/x-protobuf")
+
 
     # Sicherstellen, dass ein valides Tile-Objekt geliefert wird
     return Response(content=row[0] if row and row[0] else b"", media_type="application/x-protobuf")
