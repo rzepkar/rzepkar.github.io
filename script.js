a3a3d04b0e50f3e0de7779f3303185e5515b17a5
Rio Rzepka <rio.rzepka@gmail.com>
2025-04-27 14:48:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ let map = L.map('map', {
     maxZoom: 17,
     scrollWheelZoom: true,
     smoothWheelZoom: false,
-    smoothSensitivity: 1.3,
-	zoomControl: false // kein standard plus minus element von leaflet
+    smoothSensitivity: 1.3
+	//zoomControl: false // kein standard plus minus element von leaflet
 });
 
 // Basemaps
