613f14e5355023816b49681ed1f384c90411c670
Rio Rzepka <rio.rzepka@gmail.com>
2025-05-06 22:55:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -68,8 +68,10 @@ let rasterTiles = L.tileLayer('https://rzepkar.github.io/tiles/{z}/{x}/{y}.png',
 }).addTo(map);
 
 
-let osmb = new OSMBuildings(map).date(new Date());
-
+let osmb = new OSMBuildings(map)
+    .date(new Date())
+    .view({ tilt: 45, rotation: 60 }) // <--- wichtig
+    .load();
 
 function loadBuildings(initialLoad = false) {
     fetch('https://fastapi-heatbox.onrender.com/get_buildings')
